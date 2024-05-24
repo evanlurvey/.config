34b33bad68528eb2b642874cc0af8f41d88b9c5d
Evan <evan@myautoinventory.com>
2024-05-24 11:55:41 -0500

    update formatting on save

--- nvim/lua/custom/configs/conform.lua
+++ nvim/lua/custom/configs/conform.lua
@@ -4,22 +4,24 @@ local options = {
 
   formatters_by_ft = {
     lua = { "stylua" },
-
     javascript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescript = { "prettier" },
+    typescriptreact = { "prettier" },
     css = { "prettier" },
     html = { "prettier" },
-
     sh = { "shfmt" },
-
     go = { "goimports" },
     templ = { "templ", "rustywind" },
+
+    ["*"] = { "codespell" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 1500,
+    lsp_fallback = true,
+  },
 }
 
 require("conform").setup(options)
